b00194b8a434849d762c0be7566e3bf4358d4f28
Ryan Wells <rwells@live.com>
2017-03-13 16:52:24 -0700

    trim list form POST working now, as far as sending proper data to the trim controller, so the selected cars can be displayed

--- app/controllers/trims_controller.rb
+++ app/controllers/trims_controller.rb
@@ -4,6 +4,11 @@ class TrimsController < ApplicationController
 	end
 
 	def compare
-		
+		render plain: build_trim_list(params).inspect
 	end
+
+	private
+		def build_trim_list(my_param)
+			my_param.select! { |k,v| k.include?("trim-id-") }
+		end
 end
